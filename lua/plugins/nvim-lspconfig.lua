723f2c90c5399548cc7e9b8319cc9cfe0b2be561
Carsten Menge <menge.carsten@googlemail.com>
2026-03-11 12:21:17 +0100

    treesitter

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -11,4 +11,25 @@ return {
       enabled = true,
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
